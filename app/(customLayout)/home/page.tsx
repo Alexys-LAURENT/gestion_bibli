c210b5231ea06604191918de3b247e6978e72867
Lou <louisbw78@gmail.com>
2024-09-18 22:00:54 +0200

    [ADD] debut home page

--- app/(customLayout)/home/page.tsx
+++ app/(customLayout)/home/page.tsx
@@ -1,10 +1,16 @@
+import {Tabs, Tab} from "@nextui-org/tabs";
 
 const Page = () => {
   return (
     <div>
-      Home
+      <div className='w-4/6 flex bg-white gap-4 p-4 rounded-xl'>
+        <Tabs aria-label="Options">
+          <Tab key="foryou" title="For You"/>
+          <Tab key="mostreservedbooks" title="Most reserved books"/>
+        </Tabs>
+      </div>
     </div>
   );
-};
+}
 
 export default Page;
