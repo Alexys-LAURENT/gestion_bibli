a30dbb1dcb56cd8383380efafe17baaef2a2fc19
NealB <nealbristol2002@gmail.com>
2024-09-18 23:05:11 +0200

    boutton page

--- app/(customLayout)/my-books-rentals/page.tsx
+++ app/(customLayout)/my-books-rentals/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@nextui-org/react";
 
 const Page = () => {
   return (
-    <div>
-      My Books Rentals
+    <div style={{ display: 'flex', gap: '10px' }}>
+      <Button color="primary">On-going</Button>
+
+      <Button color="primary">Done</Button>
     </div>
   );
 };
