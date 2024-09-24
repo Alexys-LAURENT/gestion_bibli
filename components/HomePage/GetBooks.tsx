c1d07179f0a9379cf7f7963eb2dbd89fbc43f92b
Lou <louisbw78@gmail.com>
2024-09-24 10:44:49 +0200

    [ADD] better for cardlist book

--- components/HomePage/GetBooks.tsx
+++ components/HomePage/GetBooks.tsx
@@ -21,20 +21,18 @@ export default function GetBooks() {
     return (
         <div className='flex flex-wrap gap-4 py-4 w-full'>
             {books.data && books.data.map((book: any) => (
-                <Card className="grid py-4 w-[120px] h-[191px] py-0">
+                <Card className="grid py-4 w-[160px] h-[211px] py-0">
                     <CardBody className="py-0">
-                    <div className='w-[120px]'>
-                    <Image
-                        alt={book.title}
-                        className="absolute top-0 left-0 right-0 bottom-0 object-cover"
-                        src={book.image_url || '/no.jpg'}
-                        height={100}
-                        width={120}
-                    />
-                    </div>
+                        <Image
+                            alt={book.title}
+                            className="relative object-cover overflow-hidden"
+                            src={book.image_url || '/no.jpg'}
+                            height={200}
+                            width={160}
+                        />
                     </CardBody>
-                    <CardFooter className='flex-col items-end'>
-                        <p className="font-bold text-black">{book.title}</p>
+                    <CardFooter className='flex-col *items-end'>
+                        <p className="text-xs ml-2 mr-2 font-bold text-black">{book.title}</p>
                         <small className="text-default-500  text-black">{}</small>
                     </CardFooter>
                 </Card>
