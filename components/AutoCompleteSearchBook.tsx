7123a883c10176934345590685cdd08d28209f17
Louis <louis.lamouche2204@gmail.com>
2024-10-15 10:12:31 +0200

    fix autocomplete top

--- components/AutoCompleteSearchBook.tsx
+++ components/AutoCompleteSearchBook.tsx
@@ -5,12 +5,13 @@ import { Autocomplete, AutocompleteItem } from '@nextui-org/autocomplete';
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import {  useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import placeholderImg from "@/public/placeholder.png";
 const AutoCompleteSearchBook = () => {
-  const [books, setBooks] = useState<{id_book:number, title:string, image_url:string}[]>([]);
+  const [books, setBooks] = useState<{ id_book: number, title: string, image_url: string }[]>([]);
   const [autoCompleteBooksValue, setAutoCompleteBooksValue] = useState<string>('');
   const router = useRouter()
-  useEffect(() => {    
+  useEffect(() => {
     const fetchBooks = async () => {
       const { data } = await getAllBooksPaginate(0, 20, `%${autoCompleteBooksValue}%`);
       setBooks(data);
@@ -19,38 +20,38 @@ const AutoCompleteSearchBook = () => {
   }, [autoCompleteBooksValue]);
 
   const handleSelectionChange = (value: string) => {
-    if(!value) return;
+    if (!value) return;
     router.push(`/book/${value}`);
   };
 
   return (
-       <Autocomplete 
-       aria-label='Search for a book'
-       size='sm'
-       className='max-w-sm'
-       items={books}
-       value={autoCompleteBooksValue}
-        startContent={
-          <MagnifyingGlassIcon className='w-6 h-6 text-gray-400' />
-        }
-                  onValueChange={setAutoCompleteBooksValue}
-                  onSelectionChange={(value)=>handleSelectionChange(value as string)}
-                  placeholder='Search for a book...'
-                >
-                {(book) => (
-                  <AutocompleteItem textValue={book.title} value={book.id_book} key={book.id_book}>
-                    <Link href={`/book/${book.id_book}`} className='w-full flex gap-4 items-center'>
-                      <div className='flex items-center gap-2'>
-                        <Image priority={true} src={book.image_url} alt={`${book.title} - cover`} width={200} height={300} className="w-10 h-auto rounded-md" />
-                      </div>  
-                      <div className='flex flex-col gap-1'>
-                        <div className='text-sm font-semibold'>{book.title}</div>
-                      </div>
+    <Autocomplete
+      aria-label='Search for a book'
+      size='sm'
+      className='max-w-sm'
+      items={books}
+      value={autoCompleteBooksValue}
+      startContent={
+        <MagnifyingGlassIcon className='w-6 h-6 text-gray-400' />
+      }
+      onValueChange={setAutoCompleteBooksValue}
+      onSelectionChange={(value) => handleSelectionChange(value as string)}
+      placeholder='Search for a book...'
+    >
+      {(book) => (
+        <AutocompleteItem textValue={book.title} value={book.id_book} key={book.id_book}>
+          <Link href={`/book/${book.id_book}`} className='w-full flex gap-4 items-center'>
+            <div className='flex items-center gap-2'>
+              <Image priority={true} src={book.image_url ? book.image_url : placeholderImg.src} alt={`${book.title} - cover`} width={200} height={300} className="w-10 h-auto rounded-md" />
+            </div>
+            <div className='flex flex-col gap-1'>
+              <div className='text-sm font-semibold'>{book.title}</div>
+            </div>
 
-                    </Link>
-                  </AutocompleteItem>
-                )}
-              </Autocomplete>
+          </Link>
+        </AutocompleteItem>
+      )}
+    </Autocomplete>
   );
 };
 
